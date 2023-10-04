6a852991e2e7586152009f7cc9e6cd313b52e4c0
reinaldopenno <rapenno@gmail.com>
2023-10-04 14:04:24 -0700

    Update parse_firewall_log to handle more log formats.
    - Added more tests in main

--- meraki_syslog_parser/main.py
+++ meraki_syslog_parser/main.py
@@ -8,6 +8,18 @@ if __name__ == "__main__":
     parsed_log = MerakiMXSyslogParser.parse_firewall_log(fw_log_entry)
     print(parsed_log)
 
+    fw_log_entry = "Oct  2 21:11:57 192.168.128.1 1 1696306317.205815028 satosugu l7_firewall src=192.168.128.3 dst=110.242.68.66 protocol=tcp sport=51648 dport=443 decision=blocked"
+    parsed_log = MerakiMXSyslogParser.parse_firewall_log(fw_log_entry)
+    print(parsed_log)
+
+    fw_log_entry = "Oct  2 21:15:17 192.168.128.1 1 1696306517.777299778 satosugu firewall src=fe80::c7:7f6b:1f18:7fd2 dst=ff02::fb protocol=udp sport=5353 dport=5353 pattern: allow all"
+    parsed_log = MerakiMXSyslogParser.parse_firewall_log(fw_log_entry)
+    print(parsed_log)
+
+    fw_log_entry = "Oct  2 21:11:55 192.168.128.1 1 1696306315.993635266 satosugu firewall src=192.168.128.3 dst=8.43.72.24 mac=23:43:74:7e:6d:be protocol=tcp sport=40202 dport=443 pattern: allow al"
+    parsed_log = MerakiMXSyslogParser.parse_firewall_log(fw_log_entry)
+    print(parsed_log)
+
     url_log_entry = "Oct  2 21:18:42 192.168.128.1 1 1696306722.864107379 satosugu urls src=192.168.128.3:58754 dst=192.229.211.108:80 mac=0C:4D:E9:BE:F4:B2 agent='Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:109.0) Gecko/20100101 Firefox/118.0' request: POST http://ocsp.digicert.com/"
     parsed_log = MerakiMXSyslogParser.parse_url_log(url_log_entry)
     print(parsed_log)
--- meraki_syslog_parser/meraki_mx_syslog_parser.py
+++ meraki_syslog_parser/meraki_mx_syslog_parser.py
@@ -64,7 +64,7 @@ class MerakiMXSyslogParser:
             r"(?P<date>\w{3}\s+\d{1,2}\s+\d{2}:\d{2}:\d{2})\s+(?P<ip>\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})\s+\d\s+("
             r"?P<id>\d+\.\d+)\s+(?P<user>\w+)\s+(?P<type>\w+)\s+src=(?P<src>.+?)\s+dst=(?P<dst>.+?)\s+"
             r"(?:mac=(?P<mac>[0-9A-Fa-f:]{17})\s+)?"
-            r"protocol=(?P<protocol>\w+)\s+sport=(?P<sport>\d+)\s+dport=(?P<dport>\d+)\s+decision=(?P<decision>\w+)")
+            r"protocol=(?P<protocol>\w+)\s+sport=(?P<sport>\d+)\s+dport=(?P<dport>\d+)\s+(?:pattern:\s(?P<pattern>\w+)\s)?(?P<decision>.+)")
         match = re.search(pattern, log_entry)
         if match:
             return match.groupdict()
